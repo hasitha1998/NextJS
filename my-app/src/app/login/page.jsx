08e863180cf344ad9f8ae3c5df0ad91caff105b0
Hasitha Sanjaya <79785680+hasitha1998@users.noreply.github.com>
2024-10-04 14:38:08 +0530

    project setup

---
+++ my-app/src/app/login/page.jsx
@@ -0,0 +1,7 @@
+ export default function LoginPage(){
+    return(
+        <div className="bg-white">
+            <h1 className="text-center text-red-500">Hello world</h1>
+        </div>
+    );
+ }
---
+++ my-app/src/app/signup/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+import { axios } from "axios"; 
+
+
+export default function SignUpPage(){
+    return(
+        <div>
+            <h1 className="text-center text-red-500">Hello world</h1>
+        </div>
+    );
+ }
